fe787165672beb417a9a47d57b8a5683489656c5
Pralhad <mrpatilpralhad@gmail.com>
2022-02-01 17:19:38 +0530

    added coreui

--- components/login/LoginScreen.js
+++ components/login/LoginScreen.js
@@ -1,34 +1,99 @@
-import { useRouter } from 'next/router'
 
-import SocialLogin from "./SocialLogin";
-import styles from './LoginScreen.module.css';
-import Card from "../ui/Card";
-import Button from "../ui/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import {
+    CButton,
+    CCard,
+    CCardBody,
+    CCardGroup,
+    CCol,
+    CContainer,
+    CForm,
+    CFormInput,
+    CInputGroup,
+    CInputGroupText,
+    CRow,
+  } from '@coreui/react'
+  import CIcon from '@coreui/icons-react';
+  
 const LoginScreen = () => {
     const router = useRouter();
-    const signupHandler = (event) => {
-        event.preventDefault();
-        router.push('signup');
-    }
 
     const loginHandler = (event) => {
         event.preventDefault();
         router.push('dashboard');
     }
+
+    const signupHandler = (event) => {
+        event.preventDefault();
+        router.push('signup');
+    }
+
     return (
-        <Card title="Login to Survey" className={styles.input}>
-            <form className={styles.form}>
-                <label htmlFor="username" >User Name</label>
-                <input type="text" name="username"></input>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password"/>
-                <Button className={styles.loginButton} onClick={loginHandler}> Login</Button>
-                <p className={styles.signupText}>Don't have an account?</p>
-                <Button className={styles.signupButton} onClick={signupHandler}>Signup</Button>
-            </form>
-            <SocialLogin />
-        </Card>
+        <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>Login</h1>
+                    <p className="text-medium-emphasis">Sign In to your account</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        
+                      </CInputGroupText>
+                      <CFormInput placeholder="Username" autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupText>
+                      
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="Password"
+                        autoComplete="current-password"
+                      />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs={6}>
+                        <CButton color="primary" className="px-4" onClick={loginHandler}>
+                          Login
+                        </CButton>
+                      </CCol>
+                      <CCol xs={6} className="text-right">
+                        <CButton color="link" className="px-0">
+                          Forgot password?
+                        </CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>Sign up</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <Link href="/signup">
+                      <CButton color="primary" className="mt-3" active tabIndex={-1} onClick={signupHandler}>
+                        Register Now!
+                      </CButton>
+                    </Link>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+
     );
 }
-
 export default LoginScreen;
