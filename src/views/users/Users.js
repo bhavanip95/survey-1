d0bee9ba5db9bba1ef9ba0af88ba5e1b9371ce34
Pralhad <mrpatilpralhad@gmail.com>
2022-02-03 12:58:16 +0530

    sort stubs added, table modified according to requirement

--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
 import {
-  CAvatar,
   CCard,
-  CProgress,
   CTable,
   CTableBody,
   CTableDataCell,
@@ -17,179 +15,112 @@ import {
   CDropdown,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import {
-  cibCcAmex,
-  cibCcApplePay,
-  cibCcMastercard,
-  cibCcPaypal,
-  cibCcStripe,
-  cibCcVisa,
-  cifBr,
-  cifEs,
-  cifFr,
-  cifIn,
-  cifPl,
-  cifUs,
-  cilPeople,
-} from '@coreui/icons'
-
-import avatar1 from 'src/assets/images/avatars/1.jpg'
-import avatar2 from 'src/assets/images/avatars/2.jpg'
-import avatar3 from 'src/assets/images/avatars/3.jpg'
-import avatar4 from 'src/assets/images/avatars/4.jpg'
-import avatar5 from 'src/assets/images/avatars/5.jpg'
-import avatar6 from 'src/assets/images/avatars/6.jpg'
-
+import { cilSortAlphaDown } from '@coreui/icons'
 const Users = () => {
   const tableExample = [
     {
-      avatar: { src: avatar1, status: 'success' },
-      user: {
-        name: 'Yiorgos Avraamu',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'USA', flag: cifUs },
-      usage: {
-        value: 50,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Mastercard', icon: cibCcMastercard },
-      activity: '10 sec ago',
+      name: 'Bhavani Patil',
+      companyName: 'Shriram Solutions',
+      createdDate: 'Jan 1, 2021',
     },
     {
-      avatar: { src: avatar2, status: 'danger' },
-      user: {
-        name: 'Avram Tarasios',
-        new: false,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Brazil', flag: cifBr },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'info',
-      },
-      payment: { name: 'Visa', icon: cibCcVisa },
-      activity: '5 minutes ago',
+      name: 'Bhavani Patil',
+      companyName: 'Shriram Solutions',
+      createdDate: 'Jan 1, 2021',
     },
     {
-      avatar: { src: avatar3, status: 'warning' },
-      user: { name: 'Quintin Ed', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'India', flag: cifIn },
-      usage: {
-        value: 74,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'warning',
-      },
-      payment: { name: 'Stripe', icon: cibCcStripe },
-      activity: '1 hour ago',
+      name: 'Bhavani Patil',
+      companyName: 'Shriram Solutions',
+      createdDate: 'Jan 1, 2021',
     },
     {
-      avatar: { src: avatar4, status: 'secondary' },
-      user: { name: 'Enéas Kwadwo', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'France', flag: cifFr },
-      usage: {
-        value: 98,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'danger',
-      },
-      payment: { name: 'PayPal', icon: cibCcPaypal },
-      activity: 'Last month',
+      name: 'Bhavani Patil',
+      companyName: 'Shriram Solutions',
+      createdDate: 'Jan 1, 2021',
     },
     {
-      avatar: { src: avatar5, status: 'success' },
-      user: {
-        name: 'Agapetus Tadeáš',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Spain', flag: cifEs },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'primary',
-      },
-      payment: { name: 'Google Wallet', icon: cibCcApplePay },
-      activity: 'Last week',
+      name: 'Bhavani Patil',
+      companyName: 'Shriram Solutions',
+      createdDate: 'Jan 1, 2021',
     },
     {
-      avatar: { src: avatar6, status: 'danger' },
-      user: {
-        name: 'Friderik Dávid',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Poland', flag: cifPl },
-      usage: {
-        value: 43,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Amex', icon: cibCcAmex },
-      activity: 'Last week',
+      name: 'Bhavani Patil',
+      companyName: 'Shriram Solutions',
+      createdDate: 'Jan 1, 2021',
+    },
+    {
+      name: 'Bhavani Patil',
+      companyName: 'Shriram Solutions',
+      createdDate: 'Jan 1, 2021',
+    },
+    {
+      name: 'Bhavani Patil',
+      companyName: 'Shriram Solutions',
+      createdDate: 'Jan 1, 2021',
     },
   ]
+
+  const editUserHandler = (event) => {
+    event.preventDefault()
+    console.log('editing user')
+  }
+  const deleteUserHandler = (event) => {
+    event.preventDefault()
+    console.log('deleting user')
+  }
+  const sortByNameHandler = () => {
+    console.log('sort by name handler')
+  }
+  const sortByCreatedDateHandler = () => {
+    console.log('sort by created date handler')
+  }
+  const sortByCompnayNameHandler = () => {
+    console.log('sort by company name handler')
+  }
+  const sortIcon = <CIcon className="me-2" icon={cilSortAlphaDown} size="sm" />
   return (
     <CCard>
       <CTable align="middle" className="mb-0 border" hover responsive>
         <CTableHead color="light">
           <CTableRow>
-            <CTableHeaderCell className="text-center">
-              <CIcon icon={cilPeople} />
+            <CTableHeaderCell onClick={sortByNameHandler}>
+              Name
+              {sortIcon}
             </CTableHeaderCell>
-            <CTableHeaderCell>User</CTableHeaderCell>
-            <CTableHeaderCell className="text-center">Country</CTableHeaderCell>
-            <CTableHeaderCell>Usage</CTableHeaderCell>
-            <CTableHeaderCell className="text-center">Payment Method</CTableHeaderCell>
-            <CTableHeaderCell>Activity</CTableHeaderCell>
+            <CTableHeaderCell onClick={sortByCompnayNameHandler}>
+              Company Name
+              {sortIcon}
+            </CTableHeaderCell>
+            <CTableHeaderCell onClick={sortByCreatedDateHandler}>
+              Created Date
+              {sortIcon}
+            </CTableHeaderCell>
             <CTableHeaderCell>Actions</CTableHeaderCell>
           </CTableRow>
         </CTableHead>
         <CTableBody>
           {tableExample.map((item, index) => (
             <CTableRow v-for="item in tableItems" key={index}>
-              <CTableDataCell className="text-center">
-                <CAvatar size="md" src={item.avatar.src} status={item.avatar.status} />
+              <CTableDataCell>
+                <div>{item.name}</div>
               </CTableDataCell>
               <CTableDataCell>
-                <div>{item.user.name}</div>
-                <div className="small text-medium-emphasis">
-                  <span>{item.user.new ? 'New' : 'Recurring'}</span> | Registered:{' '}
-                  {item.user.registered}
-                </div>
-              </CTableDataCell>
-              <CTableDataCell className="text-center">
-                <CIcon size="xl" icon={item.country.flag} title={item.country.name} />
+                <div>{item.companyName}</div>
               </CTableDataCell>
               <CTableDataCell>
-                <div className="clearfix">
-                  <div className="float-start">
-                    <strong>{item.usage.value}%</strong>
-                  </div>
-                  <div className="float-end">
-                    <small className="text-medium-emphasis">{item.usage.period}</small>
-                  </div>
-                </div>
-                <CProgress thin color={item.usage.color} value={item.usage.value} />
-              </CTableDataCell>
-              <CTableDataCell className="text-center">
-                <CIcon size="xl" icon={item.payment.icon} />
-              </CTableDataCell>
-              <CTableDataCell>
-                <div className="small text-medium-emphasis">Last login</div>
-                <strong>{item.activity}</strong>
+                <div>{item.createdDate}</div>
               </CTableDataCell>
               <CTableDataCell>
                 <CDropdown variant="btn-group">
-                  <CDropdownToggle color="primary">Dropdown</CDropdownToggle>
+                  <CDropdownToggle color="primary">Action</CDropdownToggle>
                   <CDropdownMenu>
-                    <CDropdownItem href="#">Action</CDropdownItem>
-                    <CDropdownItem href="#">Another action</CDropdownItem>
-                    <CDropdownItem href="#">Something else here</CDropdownItem>
+                    <CDropdownItem href="#" onClick={editUserHandler}>
+                      Edit
+                    </CDropdownItem>
                     <CDropdownDivider />
-                    <CDropdownItem href="#">Separated link</CDropdownItem>
+                    <CDropdownItem href="#" onClick={deleteUserHandler}>
+                      Delete
+                    </CDropdownItem>
                   </CDropdownMenu>
                 </CDropdown>
               </CTableDataCell>
